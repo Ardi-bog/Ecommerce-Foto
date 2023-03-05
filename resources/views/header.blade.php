7a10ea5877c4a14683963a08021b0e7316dc6cfc
= <=>
2023-03-05 21:02:54 +0700

    admin and user

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,7 +13,7 @@
               </div>
           </div>
           <div class="ht-right">
-              <a href="#" class="login-panel"><i class="fa fa-user"></i>Login</a>
+              <a href="{{ url('/login') }}" class="login-panel"><i class="fa fa-user"></i>Login</a>
               <div class="lan-selector" hidden>
                   <select class="language_drop" name="countries" id="countries" style="width:300px">
                       <option value='yt' data-image="assets/img/flag-1.jpg" data-imagecss="flag yt"
@@ -25,64 +25,69 @@
           </div>
       </div>
   </div>
-  <div class="container">
-      <div class="inner-header">
-          <div class="row">
-              <div class="col-lg-2 col-md-2">
-                  <div class="logo">
-                      <a href="#">
-                          FOTOMEDIA
-                      </a>
-                  </div>
-              </div>
-              <div class="col-lg-7 col-md-7">
-                  <div class="advanced-search">
-                      <button type="button" class="category-btn">All Categories</button>
-                      <div class="input-group">
-                          <input type="text" placeholder="What do you need?" />
-                          <button type="button"><i class="ti-search"></i></button>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-  <div class="nav-item">
-      <div class="container">
-          <div class="nav-depart">
-              <div class="depart-btn">
-                  <i class="ti-menu"></i>
-                  <span>All Categories</span>
-                  <ul class="depart-hover">
-                      <li class="active"><a href="#">Wedding</a></li>
-                      <li><a href="#">Prewedding</a></li>
-                      <li><a href="#">Travel</a></li>
-                      <li><a href="#">Portrait</a></li>
-                      <li><a href="#">Lanscape</a></li>
-                      <li><a href="#">Stage</a></li>
-                      <li><a href="#">Fashion</a></li>
-                      <li><a href="#">Sport</a></li>
-                      <li><a href="#">Journalism</a></li>
-                  </ul>
-              </div>
-          </div>
-          <nav class="nav-menu mobile-menu" hidden>
-              <ul>
-                  <li><a href="/blog">Blog</a></li>
-                  <li><a href="#">Contact</a></li>
-                  <li><a href="#">Pages</a>
-                      <ul class="dropdown">
-                          <li><a href="#">Blog Details</a></li>
-                          <li><a href="#">Shopping Cart</a></li>
-                          <li><a href="#">Checkout</a></li>
-                          <li><a href="#">Faq</a></li>
-                          <li><a href="#">Register</a></li>
-                          <li><a href="#">Login</a></li>
-                      </ul>
-                  </li>
-              </ul>
-          </nav>
-          <div id="mobile-menu-wrap"></div>
-      </div>
-  </div>
+    @php
+        $link = request()->segment(1);
+    @endphp
+    @if($link != 'login')
+    <div class="container">
+        <div class="inner-header">
+            <div class="row ">
+                <div class="col-lg-2 col-md-2">
+                    <div class="logo">
+                        <a href="#">
+                            FOTOMEDIA
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-7">
+                    <div class="advanced-search">
+                        <button type="button" class="category-btn">All Categories</button>
+                        <div class="input-group">
+                            <input type="text" placeholder="What do you need?" />
+                            <button type="button"><i class="ti-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="nav-item">
+        <div class="container">
+            <div class="nav-depart">
+                <div class="depart-btn">
+                    <i class="ti-menu"></i>
+                    <span>All Categories</span>
+                    <ul class="depart-hover">
+                        <li class="active"><a href="#">Wedding</a></li>
+                        <li><a href="#">Prewedding</a></li>
+                        <li><a href="#">Travel</a></li>
+                        <li><a href="#">Portrait</a></li>
+                        <li><a href="#">Lanscape</a></li>
+                        <li><a href="#">Stage</a></li>
+                        <li><a href="#">Fashion</a></li>
+                        <li><a href="#">Sport</a></li>
+                        <li><a href="#">Journalism</a></li>
+                    </ul>
+                </div>
+            </div>
+            <nav class="nav-menu mobile-menu" hidden>
+                <ul>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Pages</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Blog Details</a></li>
+                            <li><a href="#">Shopping Cart</a></li>
+                            <li><a href="#">Checkout</a></li>
+                            <li><a href="#">Faq</a></li>
+                            <li><a href="#">Register</a></li>
+                            <li><a href="#">Login</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </div>
+    @endif
 </header>
